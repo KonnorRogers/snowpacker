dfaf321c1178be5fb7401ea9eb32858b723026bf
paramagicdev <konnor5456@gmail.com>
2020-07-19 12:30:09 -0400

    refactor: new rails 6 app with modified gitignore

--- examples/railsapp/config/routes.rb
+++ examples/railsapp/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'hello#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- examples/railsapp/test/controllers/hello_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HelloControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
