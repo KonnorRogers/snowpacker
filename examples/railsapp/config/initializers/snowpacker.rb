93555cd7d9b7462a9dcc24e14301446618df2454
ParamagicDev <konnor5456@gmail.com>
2020-07-20 18:27:37 -0400

    working on paths for configuration files

--- examples/railsapp/config/initializers/snowpacker.rb
+++ examples/railsapp/config/initializers/snowpacker.rb
@@ -1,14 +1,14 @@
-Rails.application.config.snowpacker do |_snowpacker|
+Rails.application.config.snowpacker do |snowpacker|
   # Where to find the config file
-  config.snowpacker.config_file = Rails.root.join('config', 'snowpack.config.json')
+  snowpacker.config_file = Rails.root.join('config', 'snowpack.config.json')
 
   # Where your javascript is located
-  config.snowpacker.source_path = Rails.root.join('app', 'javascript')
-  config.snowpacker.entry_points = [File.join(config.snowpacker.source_path, 'snowpacks', 'application.js')]
+  snowpacker.source_path = Rails.root.join('app', 'javascript')
+  snowpacker.entry_points = [File.join(config.snowpacker.source_path, 'snowpacks', 'application.js')]
 
   # Where to output your files to
-  config.snowpacker.out = Rails.root.join('snowpacks')
-  config.snowpacker.javascript = File.join(config.snowpacker.out, 'javascript')
-  config.snowpacker.stylesheets = File.join(config.snowpacker.out, 'stylesheets')
-  config.snowpacker.assets = File.join(config.snowpacker.out, 'assets')
+  snowpacker.out = Rails.root.join('snowpacks')
+  snowpacker.javascript = File.join(config.snowpacker.out, 'javascript')
+  snowpacker.stylesheets = File.join(config.snowpacker.out, 'stylesheets')
+  snowpacker.assets = File.join(config.snowpacker.out, 'assets')
 end
