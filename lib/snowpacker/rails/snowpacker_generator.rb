6e9ba68be56cfe10f5a93d1d72bb5ba9623b16f1
ParamagicDev <konnor5456@gmail.com>
2020-07-20 16:47:48 -0400

    rubocop all the things

--- lib/snowpacker/rails/snowpacker_generator.rb
+++ lib/snowpacker/rails/snowpacker_generator.rb
@@ -2,25 +2,12 @@
 require 'rails/generators'
 
 class SnowpackerGenerator < ::Rails::Generators::Base
+  TEMPLATES = File.join(File.expand_path("..", __dir__), 'templates')
+
   desc 'Generate Snowpacker initializer'
-
   def create_initializer_file
-    initializer 'snowpacker.rb' do <<-RUBY
-      Rails.application.config.snowpacker do |snowpacker|
-        # Where to find the config file
-        config.snowpacker.config_file = File.join('app', 'config', 'snowpack.config.json')
-
-        # Where your javascript is located
-        config.snowpacker.source_path = File.join('app', 'javascript')
-        config.snowpacker.entry_points = [File.join(config.snowpacker.source_path, 'snowpacks', 'application.js')]
-
-        # Where to output your files to
-        config.snowpacker.out = 'snowpacks'
-        config.snowpacker.javascript = File.join(config.snowpacker.out, 'javascript')
-        config.snowpacker.stylesheets = File.join(config.snowpacker.out, 'stylesheets')
-        config.snowpacker.assets = File.join(config.snowpacker.out, 'assets')
-      end
-    RUBY
+    initializer 'snowpacker.rb' do
+      File.read(File.join(TEMPLATES, 'snowpacker.rb')
     end
   end
 
@@ -34,34 +21,8 @@ class SnowpackerGenerator < ::Rails::Generators::Base
 
   desc 'Create a snowpack.config.json file'
   def create_snowpack_config_file
-    create_file File.join("app", "config", "snowpack.config.json") do <<-JSON
-      {
-        "installOptions": {
-          "NODE_ENV": true
-        },
-        "scripts": {
-          "mount:javascript": "mount app/javascript/snowpacks --to /snowpacks/javascript",
-          "mount:stylesheets": "mount app/javascript/stylesheets --to /snowpacks/stylesheets",
-          "mount:assets": "mount app/javascript/assets --to /snowpacks/assets",
-          "mount:snowpacks": "mount snowpacks --to /"
-        },
-        "devOptions": {
-          "port": 4035,
-          "open": "none",
-          "out": "snowpacks"
-        },
-        "proxy": {
-          "0.0.0.0:4035": "0.0.0.0:4035"
-        },
-        "buildOptions": {
-          "clean": true,
-          "baseUrl": "/"
-        },
-        "plugins": [
-          "@snowpack/plugin-babel"
-        ]
-      }
-    JSON
+    create_file Rails.root.join("config", "snowpack.config.json") do
+      File.read(File.join(TEMPLATES, 'snowpack.config.json'))
     end
   end
 end
--- lib/snowpacker/templates/snowpacker.rb
+++ lib/snowpacker/templates/snowpacker.rb
@@ -1,13 +1,13 @@
 Rails.application.config.snowpacker do |_snowpacker|
   # Where to find the config file
-  config.snowpacker.config_file = File.join('app', 'config', 'snowpack.config.json')
+  config.snowpacker.config_file = Rails.root.join('config', 'snowpack.config.json')
 
   # Where your javascript is located
-  config.snowpacker.source_path = File.join('app', 'javascript')
+  config.snowpacker.source_path = Rails.root.join('app', 'javascript')
   config.snowpacker.entry_points = [File.join(config.snowpacker.source_path, 'snowpacks', 'application.js')]
 
   # Where to output your files to
-  config.snowpacker.out = 'snowpacks'
+  config.snowpacker.out = Rails.root.join('snowpacks')
   config.snowpacker.javascript = File.join(config.snowpacker.out, 'javascript')
   config.snowpacker.stylesheets = File.join(config.snowpacker.out, 'stylesheets')
   config.snowpacker.assets = File.join(config.snowpacker.out, 'assets')
