e1269487acdaf8becea084fff0ade82f0f4dbf0e
ParamagicDev <konnor5456@gmail.com>
2020-07-24 12:16:54 -0400

    feat: updating templates

--- lib/snowpacker/templates/snowpacker.rb
+++ lib/snowpacker/templates/snowpacker.rb
@@ -1,8 +1,13 @@
 Snowpacker.configure do |snowpacker|
   # Where to find the config file
-  snowpacker.config_file = Rails.root.join('config', 'snowpack.config.json')
+  snowpacker.config_file = Rails.root.join('config', 'snowpack.config.js')
 
-  snowpacker.port = "4035" # Port to run on
-  snowpacker.hostname = "localhost" # hostname to use
+  snowpacker.output_path = "snowpacks" # => /public/snowpacks
+  snowpacker.port = "4035"
+
+  # Currently, snowpack does not support the hostname option as of version 2.6.4
+  # Support should be coming in 2.6.5, so until then, this is just a placeholder.
+  snowpacker.hostname = "localhost"
 end
+
 
