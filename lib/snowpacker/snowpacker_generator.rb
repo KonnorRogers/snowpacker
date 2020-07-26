1906a310eae83c3312569680454ea8decc384a1c
paramagicdev <konnor5456@gmail.com>
2020-07-26 04:48:20 -0400

    feat: generate configs with generator

--- lib/snowpacker/snowpacker_generator.rb
+++ lib/snowpacker/snowpacker_generator.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
+
 require 'rails/generators'
 
 class SnowpackerGenerator < ::Rails::Generators::Base
   TEMPLATES = File.join(File.expand_path(__dir__), 'templates')
 
   desc 'Generate Snowpacker initializer'
-  def create_initializer_files
+  def create_initializer_file
     initializer 'snowpacker.rb' do
       File.read(File.join(TEMPLATES, 'snowpacker.rb'))
     end
@@ -31,14 +32,21 @@ class SnowpackerGenerator < ::Rails::Generators::Base
                  @babel/plugin-proposal-class-properties \
                  @babel/plugin-proposal-object-rest-spread \
                  @babel/plugin-transform-runtime \
-                 @babel/plugin-transform-regenerator \
+                 @babel/plugin-transform-regenerator
     `
   end
 
-  desc 'Create a snowpack.config.json file'
-  def create_snowpack_config_file
-    create_file Rails.root.join("config", "snowpack.config.json") do
-      File.read(File.join(TEMPLATES, 'snowpack.config.json'))
+  desc 'Create config files IE: babel.config snowpack.config'
+  def create_config_files
+    config_files = %w[
+      snowpack.config.js
+      postcss.config.js
+      babel.config.js
+      .browserslistrc
+    ]
+
+    create_file Rails.root.join('config', 'snowpacker', filename) do
+      File.read(File.join(TEMPLATES, filename))
     end
   end
 end
