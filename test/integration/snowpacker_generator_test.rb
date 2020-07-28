29c79d4526e9ae5b9befab08e050b9b259917438
ParamagicDev <konnor5456@gmail.com>
2020-07-28 18:08:32 -0400

    adding test

--- test/integration/snowpacker_generator_test.rb
+++ test/integration/snowpacker_generator_test.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 class SnowpackerGeneratorTest < Minitest::Test
+  def setup
+  end
 
+  def teardown
+  end
+
+  def test_generator_works
+    Dir.chdir(RAILS_TEST_APP) { `rails generate snowpacker` }
+  end
 end
