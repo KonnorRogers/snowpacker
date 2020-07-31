f52830542234637c4d6869183e063adf3d67426b
ParamagicDev <konnor5456@gmail.com>
2020-07-31 01:08:53 -0400

    remove default initializers

--- lib/snowpacker/configuration.rb
+++ lib/snowpacker/configuration.rb
@@ -8,12 +8,5 @@ module Snowpacker
     attr_accessor :mount_dir
     attr_accessor :output_path
     attr_accessor :port, :hostname
-
-    def initialize
-      @output_path = "snowpacks"
-      @mount_dir = File.join("app", "javascript")
-      @port = "4035"
-      @hostname = "localhost"
-    end
   end
 end
