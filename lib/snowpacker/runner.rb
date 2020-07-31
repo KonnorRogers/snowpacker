a5819670c59bbdbf7ede2b89a665bfd4a5fcab38
ParamagicDev <konnor5456@gmail.com>
2020-07-31 01:16:50 -0400

    remove rails module from runner

--- lib/snowpacker/runner.rb
+++ lib/snowpacker/runner.rb
@@ -4,48 +4,46 @@ require "socket"
 require "snowpacker/env"
 
 module Snowpacker
-  module Rails
-    class Runner
-      attr_reader :config_file
+  class Runner
+    attr_reader :config_file
 
-      def initialize
-        @config_file = Snowpacker.config.config_file
-        Env.set_env_variables
-      rescue Errno::ENOENT, NoMethodError
-        $stdout.puts "Snowpacker configuration not found in #{Snowpacker.config_location}"
-        $stdout.puts "Please run bundle exec rails generate snowpacker to install Snowpacker"
-        exit!
-      end
+    def initialize
+      @config_file = Snowpacker.config.config_file
+      Env.set_env_variables
+    rescue Errno::ENOENT, NoMethodError
+      $stdout.puts "Snowpacker configuration not found in #{Snowpacker.config_location}"
+      $stdout.puts "Please run bundle exec rails generate snowpacker to install Snowpacker"
+      exit!
+    end
 
-      # Build for production
-      def build
-        snowpacker_command(env: :production, cmd: :build)
-      end
+    # Build for production
+    def build
+      snowpacker_command(env: :production, cmd: :build)
+    end
 
-      # Serve for development
-      def dev
-        detect_port!
-        snowpacker_command(env: :development, cmd: :dev)
-      end
+    # Serve for development
+    def dev
+      detect_port!
+      snowpacker_command(env: :development, cmd: :dev)
+    end
 
-      private
+    private
 
-      def snowpacker_command(env: "", cmd: "")
-        env = ENV["NODE_ENV"] || env
-        command = "NODE_ENV=#{env} yarn run snowpack #{cmd} --config #{@config_file}"
-        exec(command)
-      end
+    def snowpacker_command(env: "", cmd: "")
+      env = ENV["NODE_ENV"] || env
+      command = "NODE_ENV=#{env} yarn run snowpack #{cmd} --config #{@config_file}"
+      exec(command)
+    end
 
-      def detect_port!
-        hostname = Snowpacker.config.hostname
-        port = Snowpacker.config.port
-        server = TCPServer.new(hostname, port)
-        server.close
-      rescue Errno::EADDRINUSE
-        $stdout.puts "Another program is running on port #{port}.\n
-                      Set a new port in #{Snowpacker.config_path} for devOptions: { port: \"#{port}\" }"
-        exit!
-      end
+    def detect_port!
+      hostname = Snowpacker.config.hostname
+      port = Snowpacker.config.port
+      server = TCPServer.new(hostname, port)
+      server.close
+    rescue Errno::EADDRINUSE
+      $stdout.puts "Another program is running on port #{port}.\n
+                    Set a new port in #{Snowpacker.config_path} for devOptions: { port: \"#{port}\" }"
+      exit!
     end
   end
 end
