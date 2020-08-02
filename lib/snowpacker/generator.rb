8c4aa9d2c6bb95921d87be8ea9ccbc1d00cf62b0
paramagicdev <konnor5456@gmail.com>
2020-08-02 18:22:21 -0400

    refactoring of Generator class

--- lib/snowpacker/generator.rb
+++ lib/snowpacker/generator.rb
@@ -11,12 +11,13 @@ module Snowpacker
       babel.config.js
     ]
 
-    def in_root
+    def self.source_root
+      TEMPLATES
     end
 
     def create_initializer_file
       target = "snowpacker.rb"
-      source = File.join(TEMPLATES, "#{target}.tt")
+      source = "#{target}.tt"
 
       destination = File.join("config", "initializers", target)
 
@@ -27,7 +28,8 @@ module Snowpacker
       end
 
       # Creates a config/initializers/snowpacker.rb file
-      template File.read(source), destination
+      puts "Creating initializer file at #{destination}..."
+      template source, destination
     end
 
     def create_config_files
@@ -37,12 +39,14 @@ module Snowpacker
         destination = Rails.root.join("config", "snowpacker")
       end
 
+      puts "Creating config files @ #{destination}"
       CONFIG_FILES.each do |filename|
-        template File.join(TEMPLATES, filename), File.join(destination, filename)
+        template filename, File.join(destination, filename)
       end
     end
 
     def add_yarn_packages
+      puts "Adding yarn packages..."
       %x(yarn add -D #{::Snowpacker::YARN_PACKAGES.join(" ")})
     end
 
