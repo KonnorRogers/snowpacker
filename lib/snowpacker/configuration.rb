ca125c781ef462a87fa4e1ecbaa5ecf5f8a61968
paramagicdev <konnor5456@gmail.com>
2020-08-09 13:54:37 -0400

    allow user to add dynamic attributes

--- lib/snowpacker/configuration.rb
+++ lib/snowpacker/configuration.rb
@@ -8,5 +8,23 @@ module Snowpacker
     attr_accessor :mount_dir
     attr_accessor :output_path
     attr_accessor :port, :hostname
+
+
+    def self.add_attr(attr, value)
+      instance_attr = "@#{attr}".to_sym
+
+      define_singleton_method(attr.to_sym) { instance_variable_get(instance_attr) }
+      define_singleton_method("#{attr}=".to_sym) do |new_value|
+        instance_variable_set(instance_attr, new_value)
+      end
+
+      self.instance_variable_set("@#{attr}".to_sym, value)
+    end
+
+    # Allows for an instance version as well
+    # https://6ftdan.com/allyourdev/2015/02/24/writing-methods-for-both-class-and-instance-levels/
+    def add_attr(attr, value)
+      self.class.add_attr(attr, value)
+    end
   end
 end
