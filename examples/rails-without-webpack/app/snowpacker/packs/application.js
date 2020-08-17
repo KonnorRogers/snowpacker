ae252354f5fa32e0826d9042cc16d9bf52b27111
ParamagicDev <konnor5456@gmail.com>
2020-08-16 22:16:44 -0400

    minor tweaks

--- examples/rails-without-webpack/app/snowpacker/packs/application.js
+++ examples/rails-without-webpack/app/snowpacker/packs/application.js
@@ -1,17 +1,9 @@
-// Webpack
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
-
 import "@rails/ujs" // Autostarts
 import Turbolinks from "turbolinks"
 import ActiveStorage from "@rails/activestorage"
 import "../stylesheets/index.css"
-// import "../channels"
 
 Turbolinks.start()
 ActiveStorage.start()
 
 console.log("Hello from Snowpacker")
-
