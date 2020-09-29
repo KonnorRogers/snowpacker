0e19b1e48d44d49b9e7e215480fa617dc5cab218
ParamagicDev <konnor5456@gmail.com>
2020-09-29 13:24:19 -0400

    wip: manifest parsing

--- lib/snowpacker/manifest.rb
+++ lib/snowpacker/manifest.rb
@@ -1,9 +1,15 @@
-class Snowpacker::Manifest
-  # class << self
-  #   def parse_manifest(manifest_file = Snowpacker.config.manifest_file)
-  #   end
+require 'json'
 
-  #   def lookup(file_name, type)
-  #   end
-  # end
+class Snowpacker::Manifest
+  class << self
+    attr_accessor :manifest_hash
+
+    def parse_manifest(manifest_file = Snowpacker.config.manifest_file)
+      @manifest_hash = JSON.parse(manifest_file)
+    end
+
+    def lookup(file_name, type)
+      @manifest_hash[type][file_name]
+    end
+  end
 end
