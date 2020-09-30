8413309aa447e24546185385224a4cbdbe1b1f89
paramagicdev <konnor5456@gmail.com>
2020-09-30 14:02:44 -0400

    working on manifest lookups

--- lib/snowpacker/manifest.rb
+++ lib/snowpacker/manifest.rb
@@ -5,7 +5,7 @@ class Snowpacker::Manifest
     attr_accessor :manifest_hash
 
     def parse_manifest(manifest_file = Snowpacker.config.manifest_file)
-      @manifest_hash = JSON.parse(manifest_file)
+      @manifest_hash ||= JSON.parse(manifest_file)
     end
 
     def lookup(file_name, type)
