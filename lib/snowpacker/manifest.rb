968f05e50aaf0fee796bf739cb88b26ad57b5160
ParamagicDev <konnor5456@gmail.com>
2020-10-05 16:11:07 -0400

    tests: add manifest fixture

--- lib/snowpacker/manifest.rb
+++ lib/snowpacker/manifest.rb
@@ -1,17 +1,19 @@
 require "json"
 
-class Snowpacker::Manifest
-  class << self
-    attr_accessor :manifest_hash
+class Snowpacker
+  module Manifest
+    class << self
+      attr_accessor :manifest_hash
 
-    # rubocop:disable Naming/MemoizedInstanceVariableName
-    def parse_manifest(manifest_file = Snowpacker.config.manifest_file)
-      @manifest_hash ||= JSON.parse(manifest_file)
-    end
-    # rubocop:enable Naming/MemoizedInstanceVariableName
+      # rubocop:disable Naming/MemoizedInstanceVariableName
+      def parse_manifest(manifest_file = Snowpacker.config.manifest_file)
+        @manifest_hash ||= JSON.parse(manifest_file)
+      end
+      # rubocop:enable Naming/MemoizedInstanceVariableName
 
-    def lookup(file_name, type)
-      @manifest_hash[type][file_name]
+      def lookup(file_name, type)
+        @manifest_hash[type][file_name]
+      end
     end
   end
 end
